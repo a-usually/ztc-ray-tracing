ea604033a3bca6337dff209025e3ac751a61dc69
a-usually <404022585@qq.com>
2023-06-27 23:34:47 +0800

    task1

--- raytracer/src/main.rs
+++ raytracer/src/main.rs
@@ -143,7 +143,7 @@ fn main() {
     let width = 1200;
     let path = "output/test.jpg";
     let quality = 60; // From 0 to 100, suggested value: 60
-    let samples_per_pixel = 600;
+    let samples_per_pixel = 498;
     let max_depth = 50;
 
     // Create image data
